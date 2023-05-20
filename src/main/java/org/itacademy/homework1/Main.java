4c4a48988f6e9be0405eb0993affa602fb3227cf
Konstantin <novash.ki@gmail.com>
2023-05-20 22:57:29 +0300

    Added config, webdriver first try to open test page

--- src/main/java/org/itacademy/homework1/Main.java
+++ src/main/java/org/itacademy/homework1/Main.java
@@ -1,10 +1,11 @@
 package org.itacademy.homework1;
 
 public class Main {
-    /** Homework 1
-     *  git, selenium driver, testng  */
+
+    /**
+     * Homework 1
+     * git, selenium driver, testng
+     */
     public static void main(String[] args) {
-        System.out.println("Homework1 git, selenium driver, testng");
-        // test
     }
 }
--- src/test/java/org/itacademy/homework1/NewTest.java
+++ src/test/java/org/itacademy/homework1/NewTest.java
@@ -1,15 +1,34 @@
 package org.itacademy.homework1;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 public class NewTest {
 
-    @Test
-    public void verifyNameTest(){
-
-        SoftAssert sa = new SoftAssert();
-        sa.assertTrue(2 == 2, "not true");
+    @BeforeTest
+    public static void setup() {
+        System.out.println("BEFORE TEST");
+        Config.getProperties();
     }
 
+    @Test
+    public void verifyNameTest() {
+
+        // test TestNG soft assert
+        SoftAssert sa = new SoftAssert();
+        sa.assertTrue(true, "not true");
+
+        System.setProperty("webdriver.chrome.driver", Config.getChromeDriver());
+        WebDriver driver = new ChromeDriver();
+        driver.get(Config.getTestPage());
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        driver.quit();
+    }
 }
