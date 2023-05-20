48dc1b1b9d0c155dd80926e8e6215e2334fda614
Konstantin <novash.ki@gmail.com>
2023-05-20 14:35:01 +0300

    Project was created

---
+++ src/main/java/org/itacademy/Main.java
@@ -0,0 +1,10 @@
+package org.itacademy;
+
+public class Main {
+    /** Homework 1
+     *  git, selenium driver, testng  */
+    public static void main(String[] args) {
+        System.out.println("Homework1 git, selenium driver, testng");
+        // test
+    }
+}
